b4fd857b58c16a2903e5a9d78dfe2b43624ae498
b0ndurant <martin.aurelien01@gmail.com>
2019-05-15 11:20:19 +0200

    exo React3 le state

--- src/App.js
+++ src/App.js
@@ -1,26 +1,30 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      working: false
+    }
+  }
+
+  handleClick = () => {
+    this.setState ({ working : !this.state.working })
+  }
+  render() {
+    const work = this.state.working ? 'work' : 'App-logo';
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className={work} alt="logo" />
+          <button onClick={this.handleClick}>homer work
+        </button>
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
